855e195bd344b8f23142cffd9dbc94ba54d637f0
nick-ang <nicholas_ang@outlook.com>
2024-12-31 16:38:53 +1100

    feat(data) update cover artists

--- src/data.js
+++ src/data.js
@@ -51,7 +51,7 @@ function chillHop() {
 		{
 			name: "One Right Now",
 			cover: "https://www.dropbox.com/s/fwqo7hzo8g6b5ai/cover.jpg?raw=1",
-			artist: "NaBeats",
+			artist: "Covers",
 			audio: "https://www.dropbox.com/scl/fi/pzicra35a0i2v1xh1hdfq/01-one-right_5_ssl.wav?rlkey=7t7l8bf96jjkdjw3dzz6px9yt&raw=1",
 			color: ["#205950", "#2ab3bf"],
 			id: uuidv4(),
@@ -60,7 +60,7 @@ function chillHop() {
 		{
 			name: "Habitual",
 			cover: "https://www.dropbox.com/s/fwqo7hzo8g6b5ai/cover.jpg?raw=1",
-			artist: "NaBeats",
+			artist: "Covers",
 			audio: "https://www.dropbox.com/scl/fi/s1ito8x3tb7qn2ldyrl7d/habit_4.wav?rlkey=i6bvypihcej4ni3259w43673u&raw=1",
 			color: ["#205950", "#2ab3bf"],
 			id: uuidv4(),
@@ -69,7 +69,7 @@ function chillHop() {
 		{
 			name: "Heartless",
 			cover: "https://www.dropbox.com/s/fwqo7hzo8g6b5ai/cover.jpg?raw=1",
-			artist: "NaBeats",
+			artist: "Covers",
 			audio: "https://www.dropbox.com/scl/fi/xdjfy99mag833nn3igtjh/heartless_3.wav?rlkey=ovjhpyuezjno50riwz0e49al9&raw=1",
 			color: ["#205950", "#2ab3bf"],
 			id: uuidv4(),
@@ -78,7 +78,7 @@ function chillHop() {
 		{
 			name: "I Like That",
 			cover: "https://www.dropbox.com/s/fwqo7hzo8g6b5ai/cover.jpg?raw=1",
-			artist: "NaBeats",
+			artist: "Covers",
 			audio: "https://www.dropbox.com/scl/fi/0uoq0n9plk4o91m42ma3y/lay_4.wav?rlkey=8dgb07i5yq7bwz5sk3m1yztlh&raw=1",
 			color: ["#205950", "#2ab3bf"],
 			id: uuidv4(),
@@ -87,7 +87,7 @@ function chillHop() {
 		{
 			name: "Beautiful Mistakes",
 			cover: "https://www.dropbox.com/s/fwqo7hzo8g6b5ai/cover.jpg?raw=1",
-			artist: "NaBeats",
+			artist: "Covers",
 			audio: "https://www.dropbox.com/scl/fi/cyfcj8mtjz3p2650dxsov/mistakes_2.wav?rlkey=u5nafdw500g8wd2v97hwyzomo&raw=1",
 			color: ["#205950", "#2ab3bf"],
 			id: uuidv4(),
@@ -96,7 +96,7 @@ function chillHop() {
 		{
 			name: "Out of Time",
 			cover: "https://www.dropbox.com/s/fwqo7hzo8g6b5ai/cover.jpg?raw=1",
-			artist: "NaBeats",
+			artist: "Covers",
 			audio: "https://www.dropbox.com/scl/fi/siwo3nmgehb4st3drfnsh/out-of-time_11.wav?rlkey=kthva9jycafcstwvdgg26d38j&raw=1",
 			color: ["#205950", "#2ab3bf"],
 			id: uuidv4(),
